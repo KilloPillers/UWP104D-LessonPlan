3a675f83bbe55ba108aadaa9c1e46bf1f12c4f4c
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-11-07 15:49:38 -0800

    Initial Commit

---
+++ lessonplan.cpp
@@ -0,0 +1,40 @@
+//Standard includes
+#include <iostream>
+#include <string>
+
+int main() {
+
+  //Variable declaration
+
+  //DataType variableName = value;
+  //
+  //int - integer
+  int x = 5;
+  int y = 10;
+
+  //Prints out the value of x and y
+  std::cout << x << std::endl;
+  std::cout << y << std::endl;
+
+  //string - string of characters
+  std::string name = "John";
+  std::string lastName = "Doe";
+
+  std::cout << name << lastName << std::endl;
+ 
+
+  //float - decimal number
+  float pi = 3.14;
+  float e = 2.718;
+
+  std::cout << pi << std::endl;
+  std::cout << e << std::endl;
+
+  //bool - boolean
+  bool isTrue = true;
+  bool isFalse = false;
+
+  std::cout << isTrue << std::endl;
+  std::cout << isFalse << std::endl;
+  return 0;  
+}
