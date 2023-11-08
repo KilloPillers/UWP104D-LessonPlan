d4e96942bb3f118782130885e3dc049f84bfbc3d
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-11-07 16:29:22 -0800

    added exercise.cpp

---
+++ exercise.cpp
@@ -0,0 +1,20 @@
+//Standard includes
+#include <iostream>
+#include <string>
+
+//In todays exercise you will create a program that will declare 3 variables
+//a float that represents pi.
+//an int that represents the radius of a circle.
+//and a float that represents the area of the circle.
+//You will then calculate the area of the circle and print it to the screen
+int main() {
+  //Declare variables
+  
+  //Calculate area
+  
+  //Print area to screen
+  std::cout << "The area of the circle is: ";
+  
+  return 0;  
+}
+
