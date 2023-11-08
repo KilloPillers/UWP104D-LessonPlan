cdd409b9029c785375a5e8d0de126b123a3ade94
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-11-07 16:48:44 -0800

    added *.md files to the word counter

--- wordcounter.sh
+++ wordcounter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# This script counts the number of words in all .cpp files in the current directory
+# This script counts the number of words in all .cpp or .md files in the current directory
 total_word_count=0
 for file in ./*; do
   if [ -f "$file" ]; then
-    if [[ $file != *.cpp ]]; then
+    if [[ "$file" != *.cpp ]] && [[ "$file" != *.md ]]; then
       continue
     fi
     word_count=$(wc -w < "$file")
